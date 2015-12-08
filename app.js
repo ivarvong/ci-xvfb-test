1efd01e1d3002fcc810d51f1b04a0116f546bfda
Ivar Vong <ivar@ivarvong.com>
2015-12-08 13:06:46 -0500

    screenshot to circle artifact dir

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var Nightmare = require('nightmare');
 var vo = require('vo');
 
+
+var outputPath = "./screenshot.png";
+if (process.env.CIRCLE_ARTIFACTS !== undefined) {
+	outputPath = process.env.CIRCLE_ARTIFACTS + "/screenshot.png"
+}
+console.log('Saving to', outputPath, "...");
+
 vo(run)(function(err, result) {
   if (err) throw err;
 });
 
 function *run() {
   var nightmare = Nightmare();
+
   var title = yield nightmare
     .goto('http://www.nytimes.com')
-    .evaluate(function() {
-      return document.title;
-    });
-  console.log(title);
+    .screenshot(outputPath)
+
   yield nightmare.end();
+  console.log("...Done");
 }
