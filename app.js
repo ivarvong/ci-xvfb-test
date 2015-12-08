3de0c0cf9b02a293bded404b89384aed29913025
Ivar Vong <ivar@ivarvong.com>
2015-12-08 12:53:57 -0500

    should print NYT <title> now?

---
+++ app.js
@@ -0,0 +1,17 @@
+var Nightmare = require('nightmare');
+var vo = require('vo');
+
+vo(run)(function(err, result) {
+  if (err) throw err;
+});
+
+function *run() {
+  var nightmare = Nightmare();
+  var title = yield nightmare
+    .goto('http://www.nytimes.com')
+    .evaluate(function() {
+      return document.title;
+    });
+  console.log(title);
+  yield nightmare.end();
+}
