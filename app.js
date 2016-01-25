393933e4f88cd29387aa505a0ccf6a52b050a407
Ivar Vong <ivar@ivarvong.com>
2016-01-25 13:28:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ function *run() {
   var nightmare = Nightmare();
 
   var title = yield nightmare
+    .useragent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36")
     .viewport(7680, 4320)
     .goto('https://www.google.com/maps/@40.7508803,-74.0122352,216a,20y,56h,74.07t/data=!3m1!1e3')
-    .wait(10000)
+    .wait(20000)
     .screenshot(outputPath)
 
   yield nightmare.end();
